246648f29b198c6a1427292f8ced52215b9414de
FathimaMohi <mohideenfathima2004@gmail.com>
2024-06-24 12:57:26 +0530

    Add files via upload

---
+++ base.py
@@ -0,0 +1,43 @@
+
+#TO Print Hello World
+'''print('Hello World')'''
+x=10
+print(x)
+x=12
+y=10
+
+print('Sum of two numbers is',x+y)
+x=50
+y=45
+print('difference of two numbers is',x-y)
+
+x=24
+y=20
+print('multiple of two numbers is',x*y)
+
+x=28
+y=20
+print('divisibile of two numbers is',x/y)
+
+x=150
+y=250
+print('divisible of two numbers is',x/y)
+
+
+print(type(x))
+print(float(x))
+
+print(complex(y))
+
+x=280
+y=210
+print (float(x))
+print (type(x))
+print (complex(x))
+print (complex(y))
+print (float(y))
+print (type(y))
+x=22.2
+print (type(x))
+print (complex(x))
+print (float(x))
